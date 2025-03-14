0c36d812770e330b413654906b3a8f41df3a68ec
Virginia <v.f.cacciavillan@student.rug.nl>
2025-03-14 11:58:30 +0100

    Created vector db in Pinecone. Separated project code from test files Started README.md

---
+++ README.md
@@ -0,0 +1,12 @@
+# SmartScanner
+
+WebApp that allows the user to search for a query  in a number of files. The files are related to a specific construction project. 
+The query also find related terms in the documents. The output is a zip file with only the files with relevant information regarding the query.
+
+## Features:
+    - Create Project 
+    - Add files to Project
+    - Search through files 
+    - Create Zip files with files related to keyword
+    - Remeber previosuly searched for keywords
+
--- vector_db.py
+++ vector_db.py
@@ -1,25 +1,43 @@
 from atakan import FileHandler
-import pinecone
+from pinecone import Pinecone, ServerlessSpec
 from main import file_handler
+import numpy as np
 
-pinecone.init(api_key = 'pcsk_42coaV_3AHp5VkNqafH8yGeWY9AHXCwZij9FwfyPnjFLCrcZs7Z6Y5LErpcPb2vPWvs7R4', environment= "us-east1-gcp")
+pc = Pinecone(api_key = 'pcsk_42coaV_3AHp5VkNqafH8yGeWY9AHXCwZij9FwfyPnjFLCrcZs7Z6Y5LErpcPb2vPWvs7R4')
+
+print("Available indexes:" , pc.list_indexes())
+
 index_name = "smart-scanner-index"
-if index_name not in pinecone.list_indexes():
-    pinecone.create_index(
+if index_name not in [i["name"] for i in pc.list_indexes()]:
+    pc.create_index(
         index_name,
         dimension = 384,
-    ) # dimensions match embedding size
-index = pinecone.Index(index_name)
+        metric= "cosine",
+        spec = ServerlessSpec(
+            cloud = "aws",
+            region="us-east-1"
+        )
+    )
+else:
+    print(f"Index {index_name} already exists")
+index = pc.Index(index_name)
+
 
 def upload_embeddings(chunks, embeddings):
     vectors=[]
     for i, emb in enumerate(embeddings):
         vector_id = f"doc_{i}"
         metadata = chunks[i]["metadata"]
-        vectors.append((vector_id, emb.tolist(), {"filename": chunks[i]["metadata"].get("filename", "unknown")}))
+        vectors.append({
+            "id": vector_id,
+            "values": emb.tolist(),
+            "metadata": {"filename": chunks[i]["metadata"].get("filename", "unknown")}
+        })
     index.upsert(vectors)
+    print(f"Uploaded {len(vectors)} embeddings to Pinecone")
 
-query_vector = file_handler.embedder.encode("example query")
+query_vector = file_handler.embedder.encode("foundation")
+query_vector = query_vector.tolist()
 result = index.query(vector=query_vector, top_k=5, include_metadata=True)
 print(result)
 
