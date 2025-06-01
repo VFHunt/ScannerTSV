c79fa8ca31addbdf68ab440adf1a4fde6ad84e95
VFHunt <v.f.cacciavillan@gmail.com>
2025-06-01 18:08:53 +0200

    forgot

--- backend/app.py
+++ backend/app.py
@@ -356,5 +356,6 @@ def status_data():
         return jsonify({"error": str(e)}), 500
     
 if __name__ == "__main__":
+    app.run()
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
